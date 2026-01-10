8d9475d8d13423c2a4f0c491bcbdb987736e6897
andrexcu <andrexcu@gmail.com>
2026-01-10 20:25:30 +0800

    update profile

--- terminal_profile/Microsoft.PowerShell_profile.ps1
+++ terminal_profile/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,14 @@
-Clear-Host
+$defaultPath = "D:\projects"
 
+if (
+    -not $env:VSCODE_PID -and
+    $PWD.Path -eq "C:\Windows\System32" -and
+    (Test-Path $defaultPath)
+) {
+    Set-Location $defaultPath
+}
+
+Clear-Host
 Remove-Item Alias:ls -Force
 function l { lsd @args }
 function ls { lsd -l @args }
@@ -7,4 +16,4 @@ function la { lsd -a @args }
 function lla { lsd -la @args }
 function lt { lsd --tree @args }
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_macchiato_pro.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_macchiato_custom.omp.json" | Invoke-Expression
